81d4615a0b74319f0c7bc2a3f8c122ccfe8a3b04
Tomasz Zurkowski <tomasz.zurkowski@gmail.com>
2024-12-21 14:35:44 +0100

    Configure user in jujutsu.

--- home/programs/jujutsu.nix
+++ home/programs/jujutsu.nix
@@ -3,5 +3,11 @@
   programs.jujutsu = {
     enable = true;
     package = pkgs-unstable.jujutsu;
+    settings = {
+      user = {
+        name = "Tomasz Zurkowski";
+        email = "tomasz.zurkowski@gmail.com";
+      };
+    };
   };
 }
